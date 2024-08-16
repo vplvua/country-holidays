8143bc7f955d4487908480212eb7aa8288cc4aaa
Vasyl Pashko <vasyl.pashko@gmail.com>
2024-08-16 22:00:21 +0300

    styling app page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,20 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { RouterLinkActive, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatToolbarModule, MatTabsModule, RouterLinkActive, RouterLink],
+  imports: [
+    RouterOutlet,
+    MatToolbarModule,
+    MatTabsModule,
+    RouterLinkActive,
+    RouterLink,
+    MatSidenavModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
